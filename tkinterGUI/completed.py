91dc9a35e0c3c7698a40c44d53de2db498603110
Wei Jian <2508469t@student.gla.ac.uk>
2021-08-24 20:25:44 +0800

    Added remaning UIs

---
+++ tkinterGUI/completed.py
@@ -0,0 +1,58 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+root = Tk()
+root.title('Completed')
+root.geometry('{}x{}'.format(800, 480))
+root['background'] = '#FFFFFF'
+
+"""
+Creating and laying out all of the main containers
+"""
+# create all of the main containers
+top_frame = Frame(root, bg='white', width=800, height=250)
+bottom_frame = Frame(root, bg='white', width=800, height=130)
+
+# layout all of the main containers
+root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(0, weight=1)
+top_frame.grid(row=0, sticky="ew")
+bottom_frame.grid(row=1, sticky="ew")
+
+"""
+Creation of widgets that will be inflated onto the top frame
+"""
+# Circle decorations at the top left corner
+circle_img = (Image.open("images/corner.PNG"))
+resized_img = circle_img.resize((130, 120), Image.ANTIALIAS)
+circle = ImageTk.PhotoImage(resized_img)
+circle_img_label = Label(image=circle, bg="white")
+
+# Green tick to signify completion
+tick_img = (Image.open("images/green_tick.png"))
+resized_img = tick_img.resize((280, 280), Image.ANTIALIAS)
+tick = ImageTk.PhotoImage(resized_img)
+tick_img_label = Label(image=tick, bg="white")
+
+# Inflating the widgets in the top frame
+circle_img_label.grid(row=0, column=0, sticky=NW)
+tick_img_label.grid(row=0, column=0)
+
+
+"""
+Creation of widgets that will be inflated onto the bottom frame
+"""
+# Label for the word "Completed!"
+ngtCleaner_label = Label(root, text="Completed!", bg="white", font=("Arial bold", 18))
+
+# Start Cleaning Button
+start_btn_img = PhotoImage(file="images/startnewset_btn.PNG")
+start_btn = Button(root, image=start_btn_img,
+             borderwidth=0, bg="white", highlightthickness = 0, bd = 0)
+
+# Inflating the widgets in the bottom frame
+ngtCleaner_label.grid(row=1, column=0, sticky=N)
+start_btn.grid(row=1, column=0)
+
+root.mainloop()
---
+++ tkinterGUI/drying.py
@@ -0,0 +1,69 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+root = Tk()
+root.title('Drying')
+root.geometry('{}x{}'.format(800, 480))
+root['background'] = '#FFFFFF'
+
+"""
+Creating and laying out all of the main containers
+"""
+# create all of the main containers
+top_frame = Frame(root, bg='white', width=800, height=300)
+bottom_frame = Frame(root, bg='white', width=800, height=180)
+
+# layout all of the main containers
+root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(0, weight=1)
+top_frame.grid(row=0, sticky="ew")
+bottom_frame.grid(row=1, sticky="ew")
+
+
+"""
+Creation of widgets that will be inflated onto the frames
+"""
+# Circle decorations at the top left corner
+circle_img = (Image.open("images/corner.PNG"))
+resized_img = circle_img.resize((130, 120), Image.ANTIALIAS)
+circle = ImageTk.PhotoImage(resized_img)
+circle_img_label = Label(image=circle, bg="white")
+
+# Bubbles illustration to signifiy cleaning
+drying_img = (Image.open("images/dry.png"))
+resized_img = drying_img.resize((280, 280), Image.ANTIALIAS)
+drying = ImageTk.PhotoImage(resized_img)
+drying_img_label = Label(image=drying, bg="white")
+
+# "Drying NGT" label widget
+dryingNGT_label = Label(root, text="Drying NGT", bg="white", font=("Arial bold", 24))
+
+# Timer widget
+timer_label = Label(root, text="00:05:00", bg="white", font=("Arial bold", 24))
+
+# Stop and Pause button widget
+stop_img = (Image.open("images/stop.PNG"))
+stop = ImageTk.PhotoImage(stop_img)
+stop_btn = Button(root, image=stop,
+                       borderwidth=0, bg="white", highlightthickness = 0, bd = 0)
+
+pause_img = (Image.open("images/pause.PNG"))
+pause = ImageTk.PhotoImage(pause_img)
+pause_btn = Button(root, image=pause,
+                       borderwidth=0, bg="white", highlightthickness = 0, bd = 0 )
+# Temp & Humidity
+dht_label = Label(root, text="28°C\n30%", bg="white", font=("Arial bold", 24))
+
+# Inflating the widgets in the top frame
+circle_img_label.grid(row=0, column=0, sticky=NW)
+drying_img_label.grid(row=0, column=0, sticky=NS)
+dht_label.grid(row=0, column=0, sticky=NE)
+
+# Inflating the widgets in the bottom frame
+dryingNGT_label.grid(row=1, column=0, sticky=N)
+timer_label.grid(row=1, column=0, pady=(35,0))
+stop_btn.grid(row=1, column=0, sticky=SW)
+pause_btn.grid(row=1, column=0, sticky=SE)
+
+root.mainloop()
